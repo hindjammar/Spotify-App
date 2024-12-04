7d7f1218ea3e80f7b13fe083f9bb7afae2b2f860
Hind_Jammar <121793980+hindjammar@users.noreply.github.com>
2024-12-04 17:39:29 +0100

    Create Navigations Buttons

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -12,7 +12,7 @@ const DisplayAlbum = () => {
   return (
     <>
       <Navbar/>
-      <div className='mt-50 flex gap-8 flex-col md:flex-row md:items-end'>
+      <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
         <img className='w-48 rounded' src={albumData.image} alt="" />
           <div className="flex flex-col">
               <p>Playlist</p>
